45244f065740d14aad2bf450a206129c62989e7f
anurag.bhardwaj <abanuragbhardwaj96@gmail.com>
2023-12-15 12:50:26 +0530

    Project structure added

---
+++ Data-Structures/src/test/java/com/datastructures/DataStructures/DataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datastructures.DataStructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
